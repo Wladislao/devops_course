cf740a1ae3f0384b924fb8dee1f5dcc0d8ae21d5
Vladislav Chernyi <6942444+Wladislao@users.noreply.github.com>
2020-10-09 23:17:12 +0300

    package.json work and app src

---
+++ build/precache-manifest.4ff920ab5dd9c8da24fcac9178823752.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d32c59cb650c9db494e",
+    "url": "/Wladislao/devops_course/static/css/main.bbc72ea5.chunk.css"
+  },
+  {
+    "revision": "0d32c59cb650c9db494e",
+    "url": "/Wladislao/devops_course/static/js/main.0d32c59c.chunk.js"
+  },
+  {
+    "revision": "ace65016ad674fd8ce59",
+    "url": "/Wladislao/devops_course/static/js/runtime~main.ace65016.js"
+  },
+  {
+    "revision": "206892ec38c99bdd1291",
+    "url": "/Wladislao/devops_course/static/js/2.206892ec.chunk.js"
+  },
+  {
+    "revision": "bce24fad6d183680fb01821bd2731a7c",
+    "url": "/Wladislao/devops_course/index.html"
+  }
+];

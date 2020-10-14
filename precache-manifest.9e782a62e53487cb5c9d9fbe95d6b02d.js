9af406c25de7ebfedf7142520b928cdc0c7481ad
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-14 19:33:00 +0000

    Deploy Wladislao/devops_course to github.com/Wladislao/devops_course.git:gh-pages

---
+++ precache-manifest.9e782a62e53487cb5c9d9fbe95d6b02d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d2d968e345a0ecc41b7",
+    "url": "/Wladislao/devops_course/static/css/main.5bf59066.chunk.css"
+  },
+  {
+    "revision": "1d2d968e345a0ecc41b7",
+    "url": "/Wladislao/devops_course/static/js/main.1d2d968e.chunk.js"
+  },
+  {
+    "revision": "ace65016ad674fd8ce59",
+    "url": "/Wladislao/devops_course/static/js/runtime~main.ace65016.js"
+  },
+  {
+    "revision": "206892ec38c99bdd1291",
+    "url": "/Wladislao/devops_course/static/js/2.206892ec.chunk.js"
+  },
+  {
+    "revision": "45c6e194b6e652709439c3f7e26ef10a",
+    "url": "/Wladislao/devops_course/index.html"
+  }
+];
